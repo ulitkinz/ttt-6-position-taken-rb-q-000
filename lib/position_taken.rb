a8b9f524c6f0a58ebffb2f4d0244a4ef1a6c4c10
Tatiana Mirelez <tatianascheff@gmail.com>
2016-07-01 01:39:30 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-if board[index] == " "
+if board[index] == " " || board[index] == "" || board[index] == nil
   return false
-elsif board[index] == ""
-  return false
-elsif board[index] == nil
-return false
 else board[index] == "X" || board[index] == "O"
   return true
 end
